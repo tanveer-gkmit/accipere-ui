e4ef309b00f77f06ac0884e57299f0fd68be6f99
tanveer-gkmit <tanveer@gkmit.co>
2025-11-14 15:18:26 +0530

    Revert "Designed job listing and application components (#3)"
    This reverts commit f8d0c6a7ae783385ca1ff7770f9c02d58eeb1d4b.

--- src/components/jobs/form/FormSection.jsx
+++
@@ -1,8 +0,0 @@
-export default function FormSection({ title, children }) {
-  return (
-    <div className="space-y-3">
-      <h3 className="font-semibold text-lg">{title}</h3>
-      {children}
-    </div>
-  );
-}
