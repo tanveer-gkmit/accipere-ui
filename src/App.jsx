88ba5e71499f19e78b8123acb3652638ac081ebe
tanveer-gkmit <tanveer@gkmit.co>
2025-11-14 15:27:45 +0530

    Revert "Designed job listing and application components (#3)" (#5)
    This reverts commit f8d0c6a7ae783385ca1ff7770f9c02d58eeb1d4b.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Toaster } from "@/components/ui/sonner";
 import LoginPage from "@/pages/auth/Login"
-import Jobs from "@/pages/Index"
-
+import HelloWorld from "@/pages/HelloWorld"
 function App() {
+
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Jobs />} />
-          <Route path="/login" element={<LoginPage />} />
-        </Routes>
-      </BrowserRouter>
-      <Toaster />
+      <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<HelloWorld />} />
+        <Route path="/login" element={<LoginPage />} />
+        {/* <Route path="/register" element={<Register />} />
+        <Route path="*" element={<NotFound />} /> */}
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
